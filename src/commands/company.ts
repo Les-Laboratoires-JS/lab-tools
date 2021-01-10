4c0fc36259b0f1190bffed0e1f6d5ea008596505
Loockeeer <34001773+loockeeer@users.noreply.github.com>
2021-01-10 16:59:16 +0100

    Update company.ts

--- src/commands/company.ts
+++ src/commands/company.ts
@@ -70,8 +70,14 @@ const command: app.Command = {
       case "list": {
         const companies = app.companies.array().sort((a, b) => {
           return (
-            app.money.ensure(`company:${b.name}`, 0) -
-            app.money.ensure(`company:${a.name}`, 0)
+            company.name === "Gifi" &&
+                    !message.content.includes("paradis.fiscal")
+                      ? app.money.ensure(`gifi`, 0)
+                      : app.money.ensure(`company:${company.name}`, 0) -
+            company.name === "Gifi" &&
+                    !message.content.includes("paradis.fiscal")
+                      ? app.money.ensure(`gifi`, 0)
+                      : app.money.ensure(`company:${company.name}`, 0)
           )
         })
         const pages = await Promise.all(
