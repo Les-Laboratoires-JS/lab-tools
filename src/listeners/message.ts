45f812ab4899ee9ba8ea23af171c42a3e5559fe6
Loockeeer <34001773+loockeeer@users.noreply.github.com>
2021-06-08 20:20:32 +0200

    Update message.ts

--- src/listeners/message.ts
+++ src/listeners/message.ts
@@ -14,7 +14,7 @@ const listener: app.Listener<"message"> = {
           )
           // TODO : when we are sure it works, uncomment the line below
           //message.delete()
-        }
+        } else return message.channel.send("bite")
       }
     }
 
