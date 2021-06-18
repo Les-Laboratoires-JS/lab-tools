f6504b0e08f19bc131892ce7ba5b8a814f6d6027
loockeeer <lulu.r1313@gmail.com>
2021-06-18 19:56:29 +0200

    feat: add invite list command

---
+++ src/commands/invites.ts
@@ -0,0 +1,33 @@
+import * as app from "../app"
+
+const command: app.Command = {
+  name: "invites",
+  description: "",
+  async run(message) {
+    const guildInvites = await message.guild.fetchInvites()
+
+    return new app.Paginator(
+      app.Paginator.divider(
+        guildInvites.map((invite, index) => {
+          const name = (invite.inviter?.username ?? "inconnu").substring(0,10)
+          const rate = (
+            invite?.uses ?? 0 /
+            ((Date.now() - (invite?.createdTimestamp ?? Date.now())) / 1000 / 60 / 60 / 24 / 7)
+          ).toFixed(2)
+          return `${index+1}. ${invite.code}, par ${name}, invite ${rate} / semaine`
+        }),
+        10
+      ).map((page) => {
+        return new app.MessageEmbed()
+          .setColor("BLURPLE")
+          .setAuthor("Invites list", message.client.user?.displayAvatarURL())
+          .setDescription(page.join("\n"))
+          .setFooter(`Liste des invites du serveur ${message.guild}`)
+      }),
+      message.channel,
+      (reaction, user) => user.id === message.author.id
+    )
+  }
+}
+
+module.exports = command
